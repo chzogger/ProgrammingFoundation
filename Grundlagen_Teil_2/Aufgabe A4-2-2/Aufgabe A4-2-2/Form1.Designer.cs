c4386619f0747d6a4165057bc26e0652b56be2b2
Florian Stettler <florianstettler95.gmail.com@zbw-online.ch>
2025-11-01 16:33:13 +0100

    meine erste Ampel

--- Grundlagen_Teil_2/Aufgabe A4-2-2/Aufgabe A4-2-2/Form1.Designer.cs
+++ Grundlagen_Teil_2/Aufgabe A4-2-2/Aufgabe A4-2-2/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            UpperLight = new Button();
+            MiddleLight = new Button();
+            BottomLight = new Button();
+            Switch = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLight;
+            button1.Location = new Point(188, 122);
+            button1.Name = "button1";
+            button1.Size = new Size(235, 407);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // UpperLight
+            // 
+            UpperLight.BackColor = Color.DimGray;
+            UpperLight.Location = new Point(263, 174);
+            UpperLight.Margin = new Padding(0);
+            UpperLight.Name = "UpperLight";
+            UpperLight.Size = new Size(88, 84);
+            UpperLight.TabIndex = 1;
+            UpperLight.UseVisualStyleBackColor = false;
+            UpperLight.Click += UpperLight_Click;
+            // 
+            // MiddleLight
+            // 
+            MiddleLight.BackColor = Color.DimGray;
+            MiddleLight.Location = new Point(263, 283);
+            MiddleLight.Name = "MiddleLight";
+            MiddleLight.Size = new Size(88, 84);
+            MiddleLight.TabIndex = 2;
+            MiddleLight.UseVisualStyleBackColor = false;
+            MiddleLight.Click += MiddleLight_Click;
+            // 
+            // BottomLight
+            // 
+            BottomLight.BackColor = Color.DimGray;
+            BottomLight.Location = new Point(263, 386);
+            BottomLight.Name = "BottomLight";
+            BottomLight.Size = new Size(88, 84);
+            BottomLight.TabIndex = 3;
+            BottomLight.UseVisualStyleBackColor = false;
+            BottomLight.Click += BottomLight_Click;
+            // 
+            // Switch
+            // 
+            Switch.Location = new Point(227, 603);
+            Switch.Name = "Switch";
+            Switch.Size = new Size(187, 52);
+            Switch.TabIndex = 4;
+            Switch.Text = "Schalten";
+            Switch.UseVisualStyleBackColor = true;
+            Switch.Click += switch_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(655, 728);
+            Controls.Add(Switch);
+            Controls.Add(BottomLight);
+            Controls.Add(MiddleLight);
+            Controls.Add(UpperLight);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Ampfel";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button UpperLight;
+        private Button MiddleLight;
+        private Button BottomLight;
+        private Button Switch;
     }
 }
--- Grundlagen_Teil_2/Aufgabe A4-2-2/Aufgabe A4-2-2/Form1.cs
+++ Grundlagen_Teil_2/Aufgabe A4-2-2/Aufgabe A4-2-2/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Metrics;
+
 namespace Aufgabe_A4_2_2
 {
     public partial class Form1 : Form
@@ -6,5 +8,73 @@ namespace Aufgabe_A4_2_2
         {
             InitializeComponent();
         }
+
+        
+
+           int Colour = 0;
+                
+
+        private void switch_Click(object sender, EventArgs e)
+        {
+            if ((int)Colour == 0)
+            {
+                
+                UpperLight.BackColor = Color.Red;
+                Colour++;
+            }
+
+            else if (Colour == 1)
+            {
+                
+                MiddleLight.BackColor = Color.Orange;
+                Colour++;
+            }
+
+            else if (Colour == 2)
+            {
+                
+                UpperLight.BackColor = Color.DimGray;
+                MiddleLight.BackColor = Color.DimGray;
+                BottomLight.BackColor = Color.Green;
+                Colour++;
+            }
+
+            else if(Colour == 3)
+            {
+
+                MiddleLight.BackColor = Color.Orange;
+                BottomLight.BackColor = Color.DimGray;
+                Colour++;
+            }
+
+            else if(Colour == 4)
+            {
+                MiddleLight.BackColor = Color.DimGray;
+                BottomLight.BackColor = Color.DimGray;
+                UpperLight.BackColor = Color.Red;
+                Colour=0;
+            }
+
+
+
+
+
+
+        }
+
+        private void UpperLight_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void MiddleLight_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void BottomLight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
